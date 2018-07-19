6db5d8d8fea63059a36448e2d18aa749eb4d6831
Andrei Rusu <andrei89ro27@yahoo.co.uk>
2018-07-19 21:07:41 +0100

    clock updating itself

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+class Clock extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillMount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date:new Date()
+    });
+  }
+
+  render () {
+    return (
+      <div className="main">
+      <h1>Hello, world!</h1>
+      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<Clock />, document.getElementById('root'));
